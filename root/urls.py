d5b20aa5f14dcd5478c6da26ec934d9a82889a96
shahriyorbaxtiyorov2004 <shahriyorbaxtiyorov2004@gmail.com>
2022-12-20 00:12:54 +0500

    created products app

--- root/urls.py
+++ root/urls.py
@@ -1,18 +1,3 @@
-"""root URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -22,7 +7,7 @@ from rest_framework import permissions
 
 schema_view = views.get_schema_view(
 	openapi.Info(
-		title="Snippets API",
+		title="Kodeks24 API",
 		default_version='v1',
 		description="Test description",
 		terms_of_service="https://www.google.com/policies/terms/",
@@ -38,6 +23,7 @@ urlpatterns = [
 
 	re_path(r'^', include('accounts.urls')),
 	re_path(r'^', include('orders.urls')),
+	re_path(r'^', include('products.urls')),
 
 	re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 	re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
